afff3efb793951facca609732688043759c9437e
Darrel Pol <poldarre.dev@gmail.com>
2022-06-29 19:42:18 -0500

    adding card occurence test

--- src/services/card-occurence/card-occurence.service.spec.ts
+++ src/services/card-occurence/card-occurence.service.spec.ts
@@ -1,8 +1,16 @@
+import Card from '@/models/Card';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CardOccurenceService } from './card-occurence.service';
 
 describe('CardOccurenceService', () => {
   let service: CardOccurenceService;
+  const testCardsWith3Occurence: Card[] = [
+    new Card("AH"),
+    new Card("2S"),
+    new Card("2H"),
+    new Card("2D"),
+    new Card("JC")
+  ];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,5 +22,13 @@ describe('CardOccurenceService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('when finding an occurence of 3', () => {
+    it('should return the number of cards occurence', () => {
+      const actResult = service.evaluate(testCardsWith3Occurence);
+      const filterResut = actResult.filter(act => act.occurrence === 3);
+      expect(filterResut.length).toBe(1);
+    });
   });
 });
